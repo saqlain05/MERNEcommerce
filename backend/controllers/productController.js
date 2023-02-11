d6621481d1af33186d043bd69a7f46dec00efb3e
MD SAQLAN NASIM <roseisred9821@gmail.com>
2023-02-11 20:33:57 +0530

    First Commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+const Product = require("../models/productModel")
+
+
+// Create Product -- Admin use
+
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    });
+}
+
+//get All Products
+
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.status(201).json({
+        success: true,
+        products
+    });
+}
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { getAllProducts, createProduct } = require("../controllers/productController");
+
+const router = express.Router();
+
+router.route("/product/new").post(createProduct)
+router.route("/products").get(getAllProducts)
+
+module.exports = router
