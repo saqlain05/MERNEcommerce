fd28104c4906e38a31ea3a08544885d31aa7d15c
MD SAQLAN NASIM <roseisred9821@gmail.com>
2023-02-11 20:52:07 +0530

    Put Method

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel")
 
 
-// Create Product -- Admin use
+// Create Product -- Admin
 
 exports.createProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
@@ -20,4 +20,27 @@ exports.getAllProducts = async (req, res) => {
         success: true,
         products
     });
+}
+
+//Update Product - Admin
+
+exports.updateProduct = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product Not Found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({
+        success : true,
+        product
+    })
 }
